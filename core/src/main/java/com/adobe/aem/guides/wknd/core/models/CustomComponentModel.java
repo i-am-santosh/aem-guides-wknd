299cf4bce0433b07d41183a895c870ce1d3776a7
Santosh Sah <jjk525@corteva.com>
2024-02-13 15:07:46 +0530

    initial commit to aem

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/CustomComponentModel.java
@@ -0,0 +1,29 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.annotation.PostConstruct;
+import java.util.Optional;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+@Model(adaptables = Resource.class)
+public class CustomComponentModel {
+
+
+    @ValueMapValue
+    private String something;
+
+    public String getSomething() {
+        return something;
+    }
+
+}
